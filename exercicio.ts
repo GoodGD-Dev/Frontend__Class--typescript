60a72d46d280c1c33873296dd6a00dc33eb69b4f
GooGD.dev <dionatha.work@gmail.com>
2024-02-02 20:44:05 -0300

    first commit

---
+++ exercicio.ts
@@ -0,0 +1,16 @@
+// Função de multiplicação
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+// Função de saudação
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+// Exemplos de uso
+const resultadoMultiplicacao = multiplicar(5, 3);
+const mensagemSaudacao = saudacao("João");
+
+console.log(resultadoMultiplicacao);
+console.log(mensagemSaudacao);
